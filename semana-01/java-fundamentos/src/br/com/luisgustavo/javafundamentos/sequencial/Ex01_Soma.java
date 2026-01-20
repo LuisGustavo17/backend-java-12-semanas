170518284cbd2327e86d97f09c735cc87a98c59a
LuisGustavo17 <luisssant17@gmail.com>
2026-01-19 22:00:00 -0300

    Semana 01 - Dia 1: estrutura de package e exercício de soma (estrutura sequencial)

---
+++ semana-01/java-fundamentos/src/br/com/luisgustavo/javafundamentos/sequencial/Ex01_Soma.java
@@ -0,0 +1,20 @@
+package br.com.luisgustavo.javafundamentos.sequencial;
+
+import java.util.Scanner;
+
+public class Ex01_Soma {
+
+	public static void main(String[] args) {
+		Scanner ed = new Scanner(System.in);
+		int n1, n2;
+		
+		System.out.println("Entrada: ");
+		n1 = ed.nextInt();
+		n2 = ed.nextInt();
+		
+		System.out.println("Soma: " + (n1+n2));
+		
+
+	}
+
+}
